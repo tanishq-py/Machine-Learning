ef5ba48524d050affc9af49ebd516ed25c3b25bd
Tanishq Parihar <tanishqparihar3@gmail.com>
2024-01-02 17:49:44 +0530

    Create Tutorial_1.py

---
+++ Python_basic/Tutorial_1.py
@@ -0,0 +1,13 @@
+#variable assignment
+a=10
+type(a) #print data type of var a
+
+b=20
+print(a+b)
+print(a*b) #return a*b 
+
+firstName = "Tanishq"
+lastName = "Parihar"
+print("My first name is {} and last name is {}".format(firstName, lastName))
+
+len("Tanishq") #return size of string
